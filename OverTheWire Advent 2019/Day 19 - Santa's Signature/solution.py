c296e0a64a9756ef77da0546df609b3eb4e5e2cb
semchapeu <42423946+semchapeu@users.noreply.github.com>
2019-12-26 15:28:19 -0400

    Update solution.py

--- OverTheWire Advent 2019/Day 19 - Santa's Signature/solution.py
+++ OverTheWire Advent 2019/Day 19 - Santa's Signature/solution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from pwn import *
 from Crypto.PublicKey import RSA
-r = remote("localhost",1219)
+r = remote("3.93.128.89",1219)
 r.recvuntil("-----BEGIN PUBLIC KEY-----")
 raw_key = b"-----BEGIN PUBLIC KEY-----" + r.recvuntil("-----END PUBLIC KEY-----")
 pub = RSA.importKey(raw_key)
